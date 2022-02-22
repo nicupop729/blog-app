ec378663297ecf9c86cf878dafb21af3e48de75e
Nicolae Pop <nicupop729@gmail.com>
2022-02-22 12:15:49 -0300

    Populate seeds file and database with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create!(name: 'Nicu', photo: './assets/images/leo.jpg', bio: 'Lorry driver in UK.')
+second_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+third_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+first_post = Post.create(user_id: first_user.id, title: 'Hello', text: 'Chocolate is gooood')
+second_post = Post.create(user_id: first_user.id, title: 'Hello 2', text: 'This is my second post.')
+third_post = Post.create(user_id: second_user.id, title: 'Ola', text: 'This is my first post.')
+fourth_post = Post.create(user_id: second_user.id, title: 'Ola 2', text: 'This is my second post.')
+fifth_post = Post.create(user_id: third_user.id, title: 'Hallo', text: 'This is my first post.')
+
+Comment.create(user_id: second_user.id, post_id: first_post.id, text: 'Hi Nicu!' )
+Comment.create(user_id: third_user.id, post_id: first_post.id, text: 'Hola Matew!' )
+Comment.create(user_id: third_user.id, post_id: second_post.id, text: 'Que tal, Maxim?' )
+Comment.create(user_id: first_user.id, post_id: third_post.id, text: 'Todo Bom?' )
+Comment.create(user_id: third_user.id, post_id: third_post.id, text: 'Nice post!' )
+Comment.create(user_id: first_user.id, post_id: fifth_post.id, text: 'See you later, Lily!' )
+Comment.create(user_id: second_user.id, post_id: fifth_post.id, text: 'In a minute, please!' )
