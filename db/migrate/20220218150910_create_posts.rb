4c711d011fd6f12bee75d3f3b51055173a4943be
Nicolae Pop <nicupop729@gmail.com>
2022-02-22 12:15:06 -0300

    Refactor migration files

--- db/migrate/20220218150910_create_posts.rb
+++ db/migrate/20220218150910_create_posts.rb
@@ -2,10 +2,10 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.references :users, null: false, foreign_key: true
-      t.string :Title
-      t.text :Text
-      t.integer :CommentsCounter
-      t.integer :LikesCounter
+      t.string :title
+      t.text :text
+      t.integer :comments_counter
+      t.integer :like_counter
 
       t.timestamps
     end
--- db/migrate/20220218151044_create_comments.rb
+++ db/migrate/20220218151044_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
     create_table :comments do |t|
       t.references :users, null: false, foreign_key: true
       t.references :posts, null: false, foreign_key: true
-      t.text :Text
+      t.text :text
 
       t.timestamps
     end
