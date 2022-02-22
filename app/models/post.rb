ac2846aafb2aa0072499fbf90d55cefc6c7b9be3
Nicolae Pop <nicupop729@gmail.com>
2022-02-22 14:20:38 -0300

    Create method to update counter for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
+  has_many :likes
+  has_many :comments
+
+  after_save :update_posts_counter
+
+  private
+
+  def update_posts_counter
+    user.update(posts_counter: user.posts.length)
+  end
 end
