c44918b0b6aad8f294fcd8044f171beed5c6da3e
Nicolae Pop <nicupop729@gmail.com>
2022-02-24 17:35:47 -0300

    Implement add new comment feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,25 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @current_user = User.find(params[:user_id])
+    @post = Post.new(user: @current_user, title: params[:post][:title], text: params[:post][:text])
+    if @post.save
+      redirect_to user_posts_url({ id: @post.id }), :flash => { :success => "Your post was saved" }
+    else
+      render :new, :flash => {:error => "Something went wrong with your post"}
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
