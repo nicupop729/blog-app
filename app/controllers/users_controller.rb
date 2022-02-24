4e4a6adc871118754b2a84cee2bd7aa3a338b567
Nicolae Pop <nicupop729@gmail.com>
2022-02-24 00:56:45 -0300

    Build the user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
   def show
     @user_id = params[:id]
+    @user = User.find(@user_id)
+
+    @user_posts = Post.where(user_id: @user_id)
   end
 end
