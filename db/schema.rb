666fb56df92e5b565ef617ec726827c2c6faff12
Nicolae Pop <nicupop729@gmail.com>
2022-02-24 00:55:39 -0300

    Update code - repopulate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_21_212531) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_18_151044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
