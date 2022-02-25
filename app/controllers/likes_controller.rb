e61550f626b2ceb91a1a06df546ac1b4a3f331e2
Nicolae Pop <nicupop729@gmail.com>
2022-02-25 02:50:21 -0300

    Build the logic to like posts

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @current_user = User.find(params[:user_id])
+    @current_post = Post.find(params[:post_id])
+    is_liked = Like.where(user: @current_user, post: @current_post).exists?
+    redirect_back(fallback_location: root_path)
+    
+    if is_liked
+      flash.now[:error] = 'Error: You already liked this post'
+    else
+      @like = Like.new(user: @current_user, post: @current_post)
+      @like.save
+      flash[:success] = 'You liked this post'
+    end
+  end
+end
