7fe8bbf395cfd5874f56e379cb7190a9d1547b71
Nicolae Pop <nicupop729@gmail.com>
2022-03-01 11:50:16 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-first_user = User.create!(name: 'Nicu', photo: 'https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80', bio: 'Lorry driver in UK.')
-second_user = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=712&q=80', bio: 'Teacher from Mexico.')
-third_user = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1521146764736-56c929d59c83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.')
+first_user = User.new(:name => 'Nicu', :photo => 'https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80', :bio => 'Lorry driver in UK.',:email => 'firstUser@example.com', :password => 'password', :password_confirmation => 'password')
+second_user = User.new(:name => 'Tom', :photo => 'https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=712&q=80', :bio => 'Teacher from Mexico.',:email => 'firstUser@example.com', :password => 'password', :password_confirmation => 'password')
+third_user = User.new(:name => 'Lilly', :photo => 'https://images.unsplash.com/photo-1521146764736-56c929d59c83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', :bio => 'Teacher from Mexico.',:email => 'secondUser@example.com', :password => 'password', :password_confirmation => 'password')
+first_user.save
+second_user.save
+third_user.save
 
 first_post = Post.create(user_id: first_user.id, title: 'Hello', text: 'Chocolate is gooood')
 second_post = Post.create(user_id: first_user.id, title: 'Hello 2', text: 'This is my second post.')
