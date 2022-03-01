70ed59379743d03776d090fdcc52a1f36c21a54a
Nicolae Pop <nicupop729@gmail.com>
2022-03-01 11:49:15 -0300

    Configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do 
   root 'users#index'
+  devise_for :users, controllers: {
+    registrations: "registrations"
+  }
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
